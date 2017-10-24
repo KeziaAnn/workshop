03b676b7a97ed01bd8c1f4c55b83238f96cfc338
KeziaAnn <33048046+KeziaAnn@users.noreply.github.com>
2017-10-24 08:51:04 +0000

    Add files via upload

--- iris2.py
+++ iris2.py
@@ -1,22 +1,49 @@
-# loading library
+from scipy.spatial import distance
+
+def eucli(a,b):
+  return distance.euclidean(a,b)
+
+class myKNN():
+#fit method of Classifier
+  def fit(self,X_train,Y_train):
+    self.X_train = X_train
+    self.Y_train = Y_train
+#predict method of classifier
+  def predict(self,X_test):
+    predictions = []
+    for row in X_test:
+      labels= self.closest(row)
+      predictions.append(labels)
+    return predictions
+#closest distance
+  def closest(self,row):
+    best_dist = eucli(row,self.X_train[0])
+    best_index = 0
+    for i in range(1, len(self.X_train)):
+      dist = eucli(row,self.X_train[i])
+      if dist < best_dist:
+        best_dist =dist
+        best_index = i
+    return self.Y_train[best_index]
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
+
 iris=load_iris()
-features=iris.data
-labels=iris.target
-# instantiate learning model (k = 3)
-knn = KNeighborsClassifier()
+x=iris.data
+y=iris.target
+
+
 from sklearn.cross_validation import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(features,labels,test_size=.3)
+X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=.3)
 
 
-# fitting the model
-knn.fit(X_train, y_train)
+clf = myKNN()
+clf.fit(X_train,Y_train)
 
-# predict the response
-pred = knn.predict(X_test)
+p= clf.predict(X_test)
 
-# evaluate accuracy
+
 from sklearn.metrics import accuracy_score
-print(accuracy_score(y_test, pred))
+print("accuracy = ",accuracy_score(Y_test, p))
 
